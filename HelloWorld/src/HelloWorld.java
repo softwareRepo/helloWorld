886c46d80d4c48f92880ffe5281161bd467e7a85
Arzum <arzum.karatas.phd@gmail.com>
2016-12-22 22:30:28 +0200

    The initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+	
+		System.out.println("Hello World!");
+	}
+	
+	public static void writeName(){
+		
+		System.out.println("Its me!");
+	}
+
+
+}
