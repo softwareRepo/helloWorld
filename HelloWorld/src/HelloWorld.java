7dfee1c6cdaadd270dd84d7bb5d0d21973ad7a17
Arzum <arzum.karatas.phd@gmail.com>
2016-12-22 22:52:52 +0200

    Add a simple output change

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,11 +4,13 @@ public class HelloWorld {
 	public static void main(String[] args){
 	
 		System.out.println("Hello World!");
+		writeName();
 	}
 	
 	public static void writeName(){
 		
 		System.out.println("Its me!");
+		System.out.println("TO DO : implement");
 	}
 
 
